186c2ef9bf98d2b7fadd4dd5cc180af9c38302a2
Yusuf Ishaku <kingshakzy@gmail.com>
2023-12-15 12:10:28 +0100

    Update plinkoBoard.jsx

--- src/components/plinkoBoard.jsx
+++ src/components/plinkoBoard.jsx
@@ -110,9 +110,9 @@ export const PlinkoBoard = () => {
        
     }, [engine, score, setScore]);
     const addBall = () => {
-      const ball = Bodies.circle(376, 20, 10, {
-           restitution: 0.7,
-           friction: 0.2,
+      const ball = Bodies.circle(369, 20, 10, {
+           restitution: 0.8,
+           friction: 0.5,
            label: `ball-${20}`,
            id: new Date().getTime(),
            frictionAir: 0.05,
